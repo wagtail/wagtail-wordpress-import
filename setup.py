6ff7f6370bc01b004a41d742f7a7300b6b4a4d98
Nick Moreton <nickmoreton@me.com>
2025-05-10 15:34:54 +0100

    Allow testing with Python 3.11

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Framework :: Django",
         "Framework :: Django :: 3.1",
         "Framework :: Django :: 3.2",
